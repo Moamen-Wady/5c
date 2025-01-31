d6c09594655db44b939e23aec89190a2e303a1d5
Moamen-Wady <moamen.wady@gmail.com>
2025-02-01 01:53:30 +0200

    Final

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ export default function App() {
     if (admin) {
       setAuthorized(admin);
     }
-  }, [localStorage]);
+  }, []);
 
   const notify = useCallback((e, msg) => {
     toast[e](msg, {
