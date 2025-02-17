8eb80442893cc00e92bb3949e56d90f4783c7e60
Moamen-Wady <moamen.wady@gmail.com>
2025-02-17 20:37:46 +0200

    Final

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,16 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-build: {
-    minify: "terser",
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
-    // rollupOptions: {
-    //   external: ["react", "react-dom"],
-    // },
-  },
+// build: {
+//     minify: "terser",
+//     terserOptions: {
+//       compress: {
+//         drop_console: true,
+//         drop_debugger: true,
+//       },
+//     },
+//     rollupOptions: {
+//       external: ["react", "react-dom"],
+//     },
+//   },
 })
