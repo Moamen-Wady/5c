dc143a62acdb07c89c4f66ea153f2ef65464378c
Moamen-Wady <moamen.wady@gmail.com>
2025-02-26 01:49:54 +0200

    Final

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,19 @@
-import { lazy, Suspense, useEffect, useCallback, useState } from "react";
+import { lazy, Suspense, useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./globals.css";
 import "react-toastify/dist/ReactToastify.css";
 import { Slide, ToastContainer, toast } from "react-toastify";
 import Loading from "./Loading";
+import Errorp from "./Errorp";
+import RouteChangeHandler from "./RouteChangeHandler";
 
 const Home = lazy(() => import("./Home"));
 const Dashboard = lazy(() => import("./dashboard"));
 
 export default function App() {
   let [Authorized, setAuthorized] = useState("");
-  useEffect(() => {
-    if (navigator.userAgent.match(/samsung/i)) {
-      alert(
-        "Your browser (Samsung Internet) may not show this website's colors correctly in Dark Mode" +
-          " with setting: 'use dark mode: always/when phone dark mode is on'" +
-          "or when option: 'dark theme sites' is checked." +
-          "Please choose 'light theme websites' or consider using a standards-compliant browser instead. \n\n" +
-          "We recommend Google Chrome, Microsoft Edge, or Firefox."
-      );
-    }
-    let admin = localStorage.getItem("admin");
-    if (admin) {
-      setAuthorized(admin);
-    }
-  }, []);
-
-  const notify = useCallback((e, msg) => {
+  const [loading, setLoading] = useState(false);
+  const notify = (e, msg) => {
     toast[e](msg, {
       position: "top-center",
       autoClose: 1500,
@@ -38,41 +25,47 @@ export default function App() {
       theme: "light",
       transition: Slide,
     });
-  }, []);
+  };
 
-  const logout = useCallback(() => {
+  const logout = () => {
     localStorage.removeItem("admin");
     setAuthorized("");
-  }, []);
+  };
 
   return (
     <Router>
       <ToastContainer />
-      <Loading isPage={true} />
-      <Suspense fallback={<Loading isPage={false} />}>
-        <header>
-          <div>
+      <RouteChangeHandler setLoading={setLoading} />
+      {loading ? (
+        <Loading />
+      ) : (
+        <Suspense fallback={<Loading />}>
+          <header>
             <div>
-              <Link to="/dbrd">Admin</Link>
-              <Link to="/">Home</Link>
+              <div>
+                <Link to="/dbrd">Admin</Link>
+                <Link to="/">Home</Link>
+              </div>
+              {Authorized ? <a onClick={logout}>Log Out</a> : <></>}
             </div>
-            {Authorized ? <a onClick={logout}>Log Out</a> : <></>}
-          </div>
-        </header>
-        <Routes>
-          <Route path="/" element={<Home notify={notify} />} />
-          <Route
-            path="/dbrd"
-            element={
-              <Dashboard
-                notify={notify}
-                Authorized={Authorized}
-                setAuthorized={setAuthorized}
-              />
-            }
-          />
-        </Routes>
-      </Suspense>
+          </header>
+
+          <Routes>
+            <Route path="/" element={<Home notify={notify} />} />
+            <Route
+              path="/dbrd"
+              element={
+                <Dashboard
+                  notify={notify}
+                  Authorized={Authorized}
+                  setAuthorized={setAuthorized}
+                />
+              }
+            />
+            <Route path="*" element={<Errorp />} />
+          </Routes>
+        </Suspense>
+      )}
     </Router>
   );
 }
---
+++ src/Errorp.jsx
@@ -0,0 +1,24 @@
+export default function Errorp() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "60vh",
+      }}
+    >
+      <h1
+        style={{
+          padding:"1vw",
+          fontSize: "5vw",
+          color: "black",
+          fontFamily: "'El Messiri', sans-serif",
+          backgroundColor: "rgb(255,255,255,0.5)",
+        }}
+      >
+        Page Not Found
+      </h1>
+    </div>
+  );
+}
