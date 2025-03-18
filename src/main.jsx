8cbf2d122c6df937eb7a8149ef9ebb3c12cd4f55
Moamen-Wady <moamen.wady@gmail.com>
2025-03-18 08:40:22 +0200

    Final

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,15 @@
 import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
 import App from "./App";
+
+function rel() {
+  window.onload = () => {
+    const style = document.getElementById("main-css");
+    style.rel = "stylesheet";
+  };
+}
+window.onload = rel;
+
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
