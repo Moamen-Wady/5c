c3bbb219c0ca88adad203ff520be8c0dedbc34fe
Moamen-Wady <moamen.wady@gmail.com>
2025-03-18 08:09:38 +0200

    Final

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,35 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import compression from "vite-plugin-compression";
+import helmet from "helmet";
 
 export default defineConfig({
   plugins: [react(), compression()],
+  server: {
+    middlewareMode: "html",
+    configureServer: (server) => {
+      server.middlewares.use(
+        helmet({
+          contentSecurityPolicy: {
+            directives: {
+              defaultSrc: ["'self'"],
+              scriptSrc: ["'self'", "https://cdn.jsdelivr.net"],
+              styleSrc: [
+                "'self'",
+                "https://cdn.jsdelivr.net",
+                "https://fonts.googleapis.com",
+                "'unsafe-inline'",
+              ],
+              fontSrc: ["'self'", "https://fonts.gstatic.com", "data:"],
+              imgSrc: ["'self'", "data:"],
+              connectSrc: ["'self'", "https://5c-back.vercel.app"],
+              frameSrc: ["'none'"],
+              objectSrc: ["'none'"],
+              upgradeInsecureRequests: [],
+            },
+          },
+        })
+      );
+    },
+  },
 });
